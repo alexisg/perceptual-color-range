7a182e4ea6fd42e21bdd82e64afeb16383fabce2
Alexis Gallisá <alexis@alexisgallisa.com>
2022-08-25 12:28:01 -0400

    Add naming to color arrays.

--- src/App.js
+++ src/App.js
@@ -8,59 +8,84 @@ let hue = "285.9";
 let chr = "10";
 
 const colors = [
-  [hue, 1, 97, 1], //////grey97
-  [hue, 2, 95, 1], //////grey95
-  [hue, 4, 92, 1], //////grey93
-  [hue, 6.5, 89, 1], //////grey90
-  [hue, 8, 86, 1], /////grey88
-  [hue, 10, 79, 1], /////grey80
-  [hue, 12, 69, 1], ////grey70
-  [hue, 14, 60, 1], ////grey60
-  [hue, 15, 49, 1], ////grey50
-  [hue, 14, 38, 1], ////grey40
-  [hue, 13, 29, 1], ////grey30
-  [hue, 13, 22, 1], ////grey20
-  [hue, 12, 17.75, 1], //grey15
-  [hue, 11, 13.5, 1], ////grey12
-  [hue, 10, 9, 1], /////grey10
-  [hue, 9, 6, 1], /////grey08
-  [hue, 8, 4, 1] ///////grey05
+  { value: [hue, 0, 0, 1], name: "grey00" },
+  { value: [hue, 8, 4, 1], name: "grey05" },
+  { value: [hue, 9, 6, 1], name: "grey08" },
+  { value: [hue, 10, 9, 1], name: "grey10" },
+  { value: [hue, 11, 13.5, 1], name: "grey12" },
+  { value: [hue, 12, 17.75, 1], name: "grey15" },
+  { value: [hue, 13, 22, 1], name: "grey20" },
+  { value: [hue, 13, 29, 1], name: "grey30" },
+  { value: [hue, 14, 38, 1], name: "grey40" },
+  { value: [hue, 15, 49, 1], name: "grey50" },
+  { value: [hue, 14, 60, 1], name: "grey60" },
+  { value: [hue, 12, 69, 1], name: "grey70" },
+  { value: [hue, 10, 79, 1], name: "grey80" },
+  { value: [hue, 8, 86, 1], name: "grey88" },
+  { value: [hue, 6.5, 89, 1], name: "grey90" },
+  { value: [hue, 4, 92, 1], name: "grey93" },
+  { value: [hue, 2, 95, 1], name: "grey95" },
+  { value: [hue, 1, 97, 1], name: "grey97" }
+];
+
+// Greys No Saturation
+const colorsNoSat = [
+  { value: [hue, 0, 0, 1], name: "greyN00" },
+  { value: [hue, 0, 4, 1], name: "greyN05" },
+  { value: [hue, 0, 6, 1], name: "greyN08" },
+  { value: [hue, 0, 8.5, 1], name: "greyN10" },
+  { value: [hue, 0, 13.5, 1], name: "greyN12" },
+  { value: [hue, 0, 17.75, 1], name: "greyN15" },
+  { value: [hue, 0, 22, 1], name: "greyN20" },
+  { value: [hue, 0, 28, 1], name: "greyN30" },
+  { value: [hue, 0, 38, 1], name: "greyN40" },
+  { value: [hue, 0, 49, 1], name: "greyN50" },
+  { value: [hue, 0, 60, 1], name: "greyN60" },
+  { value: [hue, 0, 69, 1], name: "greyN70" },
+  { value: [hue, 0, 79, 1], name: "greyN80" },
+  { value: [hue, 0, 86, 1], name: "greyN88" },
+  { value: [hue, 0.5, 89, 1], name: "greyN90" },
+  { value: [hue, 0, 92, 1], name: "greyN93" },
+  { value: [hue, 0, 95, 1], name: "greyN95" },
+  { value: [hue, 0, 97, 1], name: "greyN97" }
 ];
 
 // Alpha Colors
 const colorsAlpha = [
-  [hue, chr, 97, 0.9],
-  [hue, chr, 93, 0.8],
-  [hue, chr, 87, 0.75],
-  [hue, chr, 79, 0.7],
-  [hue, chr, 70, 0.65],
-  [hue, chr, 60, 0.55],
-  [hue, chr, 49, 0.5],
-  [hue, chr, 38, 0.4],
-  [hue, chr, 27, 0.35],
-  [hue, chr, 18, 0.3],
-  [hue, chr, 11, 0.2],
-  [hue, chr, 7, 0.1]
+  { value: [hue, chr, 100, 1], name: "greyA90" },
+  { value: [hue, chr, 97, 0.9], name: "greyA90" },
+  { value: [hue, chr, 93, 0.8], name: "greyA80" },
+  { value: [hue, chr, 87, 0.75], name: "greyA70" },
+  { value: [hue, chr, 79, 0.7], name: "greyA60" },
+  { value: [hue, chr, 70, 0.65], name: "greyA50" },
+  { value: [hue, chr, 60, 0.55], name: "greyA40" },
+  { value: [hue, chr, 49, 0.5], name: "greyA30" },
+  { value: [hue, chr, 38, 0.4], name: "greyA20" },
+  { value: [hue, chr, 27, 0.35], name: "greyA15" },
+  { value: [hue, chr, 18, 0.3], name: "greyA10" },
+  { value: [hue, chr, 11, 0.2], name: "greyA05" },
+  { value: [hue, chr, 7, 0.1], name: "greyA00" }
 ];
 
 const colorsOld = [
-  [290.29, 1.53, 96.61],
-  [290.45, 3.59, 94.98],
-  [286.72, 5.17, 92.53],
-  [288.26, 7.81, 89.15],
-  [289.16, 10.49, 86.1],
-  [289.65, 12.29, 79.0],
-  [290.32, 14.87, 68.09],
-  [286.39, 14.92, 56.03],
-  [290.63, 14.27, 45.93],
-  [289.24, 13.74, 35.06],
-  [288.83, 11.95, 24.02],
-  [290.85, 11.58, 19.98],
-  [288.38, 10.53, 16.06],
-  [287.76, 9.43, 12.11],
-  [289.96, 8.28, 8.12],
-  [287.85, 7.33, 6.17],
-  [290.17, 6.79, 3.97]
+  { value: [290.17, 0, 0], name: "greyOld00" },
+  { value: [290.17, 6.79, 3.97], name: "greyOld05" },
+  { value: [287.85, 7.33, 6.17], name: "greyOld08" },
+  { value: [289.96, 8.28, 8.12], name: "greyOld10" },
+  { value: [287.76, 9.43, 12.11], name: "greyOld12" },
+  { value: [288.38, 10.53, 16.06], name: "greyOld15" },
+  { value: [290.85, 11.58, 19.98], name: "greyOld20" },
+  { value: [288.83, 11.95, 24.02], name: "greyOld30" },
+  { value: [289.24, 13.74, 35.06], name: "greyOld40" },
+  { value: [290.63, 14.27, 45.93], name: "greyOld50" },
+  { value: [286.39, 14.92, 56.03], name: "greyOld60" },
+  { value: [290.32, 14.87, 68.09], name: "greyOld70" },
+  { value: [289.65, 12.29, 79.0], name: "greyOld80" },
+  { value: [289.16, 10.49, 86.1], name: "greyOld88" },
+  { value: [288.26, 7.81, 89.15], name: "greyOld90" },
+  { value: [286.72, 5.17, 92.53], name: "greyOld93" },
+  { value: [290.45, 3.59, 94.98], name: "greyOld95" },
+  { value: [290.29, 1.53, 96.61], name: "greyOld97" }
 ];
 
 function chromaHCL(color) {
@@ -76,7 +101,7 @@ function a11y(color) {
   }
 }
 
-function colorOutput(color, colorPrev, colorNext, i) {
+function colorOutput(color, colorPrev, colorNext, i, colorName) {
   return (
     <>
       <div
@@ -86,7 +111,7 @@ function colorOutput(color, colorPrev, colorNext, i) {
           color: a11y(chromaHCL(color).hex())
         }}
       >
-        {i}
+        {colorName}
         <p>
           hsla(
           {chromaHCL(color).get("hsl.h").toFixed(2)},
@@ -138,21 +163,67 @@ function colorOutput(color, colorPrev, colorNext, i) {
   );
 }
 
-function styleOutput(color, i) {
-  return <>{"--grey" + i + ":" + chromaHCL(color).hex() + ";"}</>;
+function styleOutput(color, i, colorName) {
+  return <>{"--" + colorName + ": rgba(" + chromaHCL(color).rgba() + ");"}</>;
 }
 
-const styleList = colors.map((color, i) => <>{styleOutput(color, i)}</>);
+const styleList = colors.map((color, i) => (
+  <>{styleOutput(color.value, i, color.name)}</>
+));
+const styleListNoSat = colorsNoSat.map((color, i) => (
+  <>{styleOutput(color.value, i, color.name)}</>
+));
+
+const styleListOld = colorsOld.map((color, i) => (
+  <>{styleOutput(color.value, i, color.name)}</>
+));
 
 const colorListOld = colorsOld.map((color, i) => (
-  <>{colorOutput(color, colorsOld[i - 1], colorsOld[i + 1], i)}</>
+  <>
+    {colorOutput(
+      color.value,
+      colorsOld[i - 1]?.value,
+      colorsOld[i + 1]?.value,
+      i,
+      color.name
+    )}
+  </>
 ));
 
 const colorList = colors.map((color, i) => (
-  <>{colorOutput(color, colors[i - 1], colors[i + 1], i)}</>
+  <>
+    {colorOutput(
+      color.value,
+      colors[i - 1]?.value,
+      colors[i + 1]?.value,
+      i,
+      color.name
+    )}
+  </>
 ));
-const colorList2 = colorsAlpha.map((color, i) => (
-  <>{colorOutput(color, colorsAlpha[i - 1], colorsAlpha[i + 1], i)}</>
+
+const colorListNoSat = colorsNoSat.map((color, i) => (
+  <>
+    {colorOutput(
+      color.value,
+      colorsNoSat[i - 1]?.value,
+      colorsNoSat[i + 1]?.value,
+      i,
+      color.name
+    )}
+  </>
+));
+
+const colorListAlpha = colorsAlpha.map((color, i) => (
+  <>
+    {colorOutput(
+      color.value,
+      colorsAlpha[i - 1]?.value,
+      colorsAlpha[i + 1]?.value,
+      i,
+      color.name
+    )}
+  </>
 ));
 
 export default function App() {
@@ -162,14 +233,71 @@ export default function App() {
         <style>
           {":root{"}
           {styleList}
+          {styleListNoSat}
+          {styleListOld}
           {"}"}
         </style>
+
+        <div class="split">
+          <div class="old">
+            <div className="panel">
+              <div className="card">
+                <div className="card__media">
+                  <img src="IMG_1893.jpeg" alt="" />
+                </div>
+                <div className="card__chin">
+                  <h2 className="card__title">Churro-with-a-big-stick.MOV</h2>
+                  <p className="card__deck">Alexis Gallisá • July 4th 2022</p>
+                </div>
+              </div>
+              <div className="card">
+                <div className="card__media">
+                  <img src="IMG_1893.jpeg" alt="" />
+                </div>
+                <div className="card__chin">
+                  <h2 className="card__title">Churro-with-a-big-stick.MOV</h2>
+                  <p className="card__deck">Alexis Gallisá • July 4th 2022</p>
+                  <div className="card__meta">
+                    <p className="card__deck">Reviewer</p>
+                    <p className="card__deck">Bob Ross</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="panel">
+            <div className="card">
+              <div className="card__media">
+                <img src="IMG_1893.jpeg" alt="" />
+              </div>
+              <div className="card__chin">
+                <h2 className="card__title">Churro-with-a-big-stick.MOV</h2>
+                <p className="card__deck">Alexis Gallisá • July 4th 2022</p>
+              </div>
+            </div>
+            <div className="card">
+              <div className="card__media">
+                <img src="IMG_1893.jpeg" alt="" />
+              </div>
+              <div className="card__chin">
+                <h2 className="card__title">Churro-with-a-big-stick.MOV</h2>
+                <p className="card__deck">Alexis Gallisá • July 4th 2022</p>
+                <div className="card__meta">
+                  <p className="card__deck">Reviewer</p>
+                  <p className="card__deck">Bob Ross</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <div
           style={{
             paddingBottom: "24px"
           }}
         >
-          <h1>Perceptual Colors</h1>
+          {/* <h1>Perceptual Colors</h1>
           <p>
             What hcl hue is 232? hsl(232, 22%, 11%) = hcl(
             {chroma("hsl(232,22%,11%)").get("hcl.h").toFixed(4)},
@@ -187,7 +315,7 @@ export default function App() {
             {chroma("rgba(13, 13, 24, 0.1)").get("hcl.h").toFixed(4)},
             {chroma("rgba(13, 13, 24, 0.1)").get("hcl.c").toFixed(4)},
             {chroma("rgba(13, 13, 24, 0.1)").get("hcl.l").toFixed(4)})
-          </p>
+          </p> */}
         </div>
         <div className="cols">
           <div>
@@ -196,39 +324,23 @@ export default function App() {
           </div>
 
           <div>
-            <h2>Greys</h2>
+            <h2>Greys Adjusted</h2>
             {colorList}
+          </div>
+
+          <div>
+            <h2>Greys No Sat</h2>
+            {colorListNoSat}
           </div>
 
           <div
             style={{
               padding: "32px",
-              background: "#1D1F29"
+              background: "var(--grey10)"
             }}
           >
             <h2>Alpha Greys</h2>
-            <div>{colorList2}</div>
-          </div>
-        </div>
-
-        <div className="panel">
-          <div className="card">
-            <div className="card__media">
-              <img src="IMG_1893.jpeg" alt="" />
-            </div>
-            <div className="card__chin">
-              <h2 className="card__title">Churro-with-a-big-stick.MOV</h2>
-              <p className="card__deck">Alexis Gallisá • July 4th 2022</p>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card__media">
-              <img src="IMG_1893.jpeg" alt="" />
-            </div>
-            <div className="card__chin">
-              <h2 className="card__title">Churro-with-a-big-stick.MOV</h2>
-              <p className="card__deck">Alexis Gallisá • July 4th 2022</p>
-            </div>
+            <div>{colorListAlpha}</div>
           </div>
         </div>
       </div>
